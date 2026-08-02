32d35b5a647cc3c1686883106acfda03d76b9372
nurarifahr <202030354@mhs.udb.ac.id>
2026-08-02 14:42:51 +0700

    styling admin

---
+++ app/(auth)/admin/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../../globals.css";
+
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  display: "swap",
+  weight: ["400","500","600","700","800"]
+});
+
+
+export const metadata: Metadata = {
+  title: "SportOn Admin",
+  description: "Engineered for endurance and designed for speed. Experience gear that moves as fast as you do. Premium fabrics. Unmatched comfort. Limitless motion.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${poppins.variable} antialiased`}
+    >
+      <body className="min-h-full flex flex-col" cz-shortcut-listen="true">
+        {children}
+        </body>
+    </html>
+  );
+}
